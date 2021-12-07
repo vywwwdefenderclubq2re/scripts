0726bbd1b7d6a1e832ab11f718691a2426b413bf
vywwwdefenderclubq2re <95682672+vywwwdefenderclubq2re@users.noreply.github.com>
2021-12-07 10:36:06 +0100

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,2 +1,4 @@
-document.body.style.backgroundColor = 'red';
-document.querySelector('h3').textContent = 'pwned';
+window.onload = function () {
+  document.body.style.backgroundColor = 'red';
+  document.querySelector('h3').textContent = 'pwned';
+}
